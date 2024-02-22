8813b8aa4f4605863a82ca234bcd6d5218f765d3
kelechiopurum <157651060+kelechiopurum@users.noreply.github.com>
2024-02-21 23:58:44 -0500

    Update and rename PA1.java to Main.java

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+      Hand playersJand = new Hand();
+      Hand dealersHand = new Hand();
+
+      //add cards to the palyer's hand
+      playersHand.add("3D");
+      playersHand.add("(9C");
+      playersHand.display();
+      System.out.println("Total: " + playersHand.total());
+      
+      //add cards to the dealer's hand
+      dealersHand.add("TC");
+      dealersHand.add("XX");
+      dealersHand.display();
+      System.out.println("Total: "+ dealersHand.total());
+    }
+}
